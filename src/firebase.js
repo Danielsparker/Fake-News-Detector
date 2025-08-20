94407aa7c002388b47f0116a0c8c66701acd0802
Daniel <danieldec996@gmail.com>
2025-08-20 22:01:27 +0530

    api key added

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,21 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAfR_MAolgrJ-bNO71yzbMvpyd8xa6DJpA",
-  authDomain: "fakebuster-ai.firebaseapp.com",
-  projectId: "fakebuster-ai",
-  storageBucket: "fakebuster-ai.firebasestorage.app",
-  messagingSenderId: "1061676619049",
-  appId: "1:1061676619049:web:1569f6431900987055d338",
-  measurementId: "G-Y9SC0YQCYN"
+  apiKey: "AIzaSyApr4qkS8MYSkxS-V4Tz9J209PCPwR1Ltc",
+  authDomain: "fake-buster-caed9.firebaseapp.com",
+  projectId: "fake-buster-caed9",
+  storageBucket: "fake-buster-caed9.firebasestorage.app",
+  messagingSenderId: "215697454781",
+  appId: "1:215697454781:web:c60e65d0429fbeab69a9d4",
+  measurementId: "G-ZFH71TTRSQ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export { app, auth, provider };
+const analytics = getAnalytics(app);
